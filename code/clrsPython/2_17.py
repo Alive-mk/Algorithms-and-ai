23473e143d31d8301eefac17dce2eab743590055
BIT-Ming <2924565971@qq.com>
2025-02-17 16:20:32 +0800

    Update 2_17.py

--- code/clrsPython/2_17.py
+++ code/clrsPython/2_17.py
@@ -75,4 +75,12 @@ class Solution:
             
         traverse(root, 0)
         return levels
+    
+    def invertTree(self, root:TreeNode):
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
                 
