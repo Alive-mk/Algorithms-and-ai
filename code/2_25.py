510b8555d1814428c2cb95141ea74401e4eb0f28
BIT-Ming <2924565971@qq.com>
2025-02-25 17:21:44 +0800

    Update 2_25.py

--- code/2_25.py
+++ code/2_25.py
@@ -23,14 +23,39 @@
 
 # tokenizer.decode(encoded_text['input_ids'])
 
-from transformers import AutoTokenizer
+# from transformers import AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-cased")
+# tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-cased")
 
-batch_sentences = [
-    ["But what about second breakfast?","i am a sentence"],
-    "Don't think he knows about second breakfast, Pip.",
-    "What about elevensies?",
-]
-encoded_input = tokenizer(batch_sentences, padding=True, truncation = True)
-print(encoded_input)
+# batch_sentences = [
+#     ["But what about second breakfast?","i am a sentence"],
+#     "Don't think he knows about second breakfast, Pip.",
+#     "What about elevensies?",
+# ]
+# encoded_input = tokenizer(batch_sentences, padding=True, truncation = True)
+# print(encoded_input)
+
+# {'input_ids': [[101, 1252, 1184, 1164, 1248, 6462, 136, 102, 178, 1821, 170, 5650'to, 102, 0, 0], [101, 1790, 112, 189, 1341, 1119, 3520, 1164, 1248, 6462, 117, 2190 0,2, 1643, 119, 102], [101, 1327, 1164, 5450, 23434, 136, 102, 0, 0, 0, 0, 0, 0, 0,ion 0]], 
+# 'token_type_ids': [[0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0], [0, 0, 0,1,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], 
+# 'attention_mask': [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]]}
+
+class Solution:
+    def combinationSum3(self, k, n):
+        result = []
+        self.backtracking(n, k, 0, 1, [], result)
+        return result
+    
+    def backtracking(self, targetSum, k, currentSum, startIndex, path, result):
+        if currentSum > targetSum:
+            return 
+        
+        if len(path) == k:
+            if currentSum == targetSum:
+                result.append(path[:])
+            return
+        for i in range(startIndex, 9 - (k - len(path)) + 2):
+            currentSum += i
+            path.append(i)
+            self.backtracking(targetSum, k, currentSum + i, i + 1, path, result)
+            currentSum -= i
+            path.pop()
